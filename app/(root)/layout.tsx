d586407f38d9b6c7698742c25147cd4f2d62ff67
CompCodeHub <Suyashtalekar2000@gmail.com>
2025-02-13 20:40:37 -0500

    Added header and footer components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from "@/components/footer";
+import Header from "@/components/shared/header";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,9 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex flex-col h-screen">
-        <main className="flex-1 wrapper">
-            {children}
-        </main>
+      <Header />
+      <main className="flex-1 wrapper">{children}</main>
+      <Footer />
     </div>
   );
 }
