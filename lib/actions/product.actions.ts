5ba855d144dd9b2ee7064bc5ccf11e76f4bb44c9
CompCodeHub <Suyashtalekar2000@gmail.com>
2025-02-22 21:27:32 -0500

    Setup zod validation and type inference

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,6 +3,7 @@
 import { PrismaClient } from "@prisma/client";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { Decimal } from "@prisma/client/runtime/library";
 
 // Get latest products
 export async function getLatestProducts() {
