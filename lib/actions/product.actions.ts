7b36214ca3eb0229886a12329158acf836d1f9f5
CompCodeHub <Suyashtalekar2000@gmail.com>
2025-02-22 22:00:57 -0500

    Configured app for serverless environment(vercel)

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,11 +3,10 @@
 import { PrismaClient } from "@prisma/client";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
-import { Decimal } from "@prisma/client/runtime/library";
+import { prisma } from "@/db/prisma";
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
 
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
