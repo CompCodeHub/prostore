7a1711d8591d348ab1c48d4b80e84f70af1ee0b4
CompCodeHub <Suyashtalekar2000@gmail.com>
2025-02-23 20:15:09 -0500

    Implemented product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -4,14 +4,19 @@ import { PrismaClient } from "@prisma/client";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 import { prisma } from "@/db/prisma";
+import { Product } from "@/types";
 
 // Get latest products
 export async function getLatestProducts() {
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
   return convertToPlainObject(data);
+}
+
+// Get single product by its slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({ where: { slug: slug } });
 }
